e1ee23a552335768bf2a744135a629cfc16a8ae3
Lubos Marton <lubos.marton@accenture.com>
2021-06-25 12:56:04 +0200

    first

---
+++ src/pages/Login/index.js
@@ -0,0 +1,13 @@
+import React, { PureComponent } from 'react'
+
+export class index extends PureComponent {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
+
+export default index
